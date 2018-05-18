ea8a6afa354a563b45c2617d71587207d9d6359a
Sergey Nechaev <sergey.nechaev@equifax.com>
2018-05-18 16:23:22 +1000

    decentralization WIP

--- src/main/java/io/zdp/api/model/v1/Urls.java
+++ src/main/java/io/zdp/api/model/v1/Urls.java
@@ -8,9 +8,9 @@ public class Urls {
 
 	public static final String URL_GET_NEW_ACCOUNT = "/api/account/new";
 
-	public static final String URL_GET_BALANCE = "/api/v1/account/balance";
+	public static final String URL_GET_BALANCE = "/api/account/balance";
 
-	public static final String URL_TRANSFER = "/api/v1/transfer";
+	public static final String URL_TRANSFER = "/api/transfer";
 
 	public static final String URL_GET_TX_DETAILS = "/api/tx/";
 
